b80db8eefaeb2c92ba0369bdb1293c00fcfff32c
joaomateusmm <joaomateusmb@gmail.com>
2026-09-24 21:47:27 -0300

    fix 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import HeroSection from "@/components/HeroSection";
 import ProductCatalog from "@/components/ProductCatalog";
 import TestimonialsSection from "@/components/TestimonialsSection";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div className="font-montserrat relative flex min-h-screen flex-col overflow-x-hidden bg-[#010000] text-white">
